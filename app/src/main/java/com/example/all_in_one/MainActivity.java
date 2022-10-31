e64062951a10f0d50ae64ed398623279d5f9eb7c
VouttaRene <45213212+VouttaRene@users.noreply.github.com>
2022-10-31 15:07:29 +0100

    Test23

--- app/src/main/java/com/example/all_in_one/MainActivity.java
+++ app/src/main/java/com/example/all_in_one/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Test Kommentar2
+    //Test Kommentar23
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
