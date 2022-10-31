19abc2219fdc80f633e31a0f26dff4cce8916ef0
FranzRL <117094240+FranzRL@users.noreply.github.com>
2022-10-31 16:37:28 +0100

    Du HUND

--- app/src/main/java/com/example/all_in_one/MainActivity.java
+++ app/src/main/java/com/example/all_in_one/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Test Kommentar23 Du HUND
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+        Button notes = findViewById(R.id.notes);
+        Button calender = findViewById(R.id.calender);
+        Button alarm =  findViewById(R.id.alarm);
+        Button sport = findViewById(R.id.sport);
+
+   }
 }
